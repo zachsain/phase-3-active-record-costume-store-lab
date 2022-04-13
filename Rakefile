35a3b7eaafb2d6ce7bf487fce9c2e083a7e4d357
Zachary Sain <zachsain@gmail.com>
2022-04-13 11:56:40 -0500

    commit tree b3088cd4c2d608a28c0e5eb190cdd9cbc21f6c48

--- Rakefile
+++ Rakefile
@@ -9,3 +9,6 @@ task :console do
   # Open a Pry session
   Pry.start
 end
+
+
+# bundle exec rake db:create_migration NAME=create_costumes
--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,5 @@
 # Create your HauntedHouse class here
+class HauntedHouse < ActiveRecord::Base
+end 
+
+# bundle exec rake db:create_migration NAME=haunted_houses
